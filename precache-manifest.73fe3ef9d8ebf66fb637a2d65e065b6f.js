8d4d67330e92cb4f0486ac76e257ec8615b8f942
aleksarad <alexa.radevich1@gmail.com>
2020-08-25 18:49:04 -0400

    Updates

---
+++ precache-manifest.73fe3ef9d8ebf66fb637a2d65e065b6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b718fe4ad24994d9adf1c7313f5bb2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df3f591d994793f5837d",
+    "url": "/static/css/main.bc47f91e.chunk.css"
+  },
+  {
+    "revision": "7aec075e5b11d7de759b",
+    "url": "/static/js/2.d701ee69.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d701ee69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df3f591d994793f5837d",
+    "url": "/static/js/main.ef7db320.chunk.js"
+  },
+  {
+    "revision": "e8fa21994f6caf9d7ee2",
+    "url": "/static/js/runtime-main.d1ba5111.js"
+  },
+  {
+    "revision": "834617915f435cb69bd361f24d063c5f",
+    "url": "/static/media/avatar2.83461791.png"
+  }
+]);
