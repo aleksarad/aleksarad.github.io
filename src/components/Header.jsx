8c2c7b779e844aa5bb6412d7bc357c651c92b7f3
aleksarad <alexa.radevich1@gmail.com>
2020-09-08 18:39:02 -0400

    layout updated and added selected link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,38 @@
 import React from 'react';
-import { Link, useRouteMatch} from 'react-router-dom';
-import avatar from "../images/avatar2.png"
+import { Link, useRouteMatch, Redirect} from 'react-router-dom';
+import { TransitionGroup, CSSTransition, Transition } from "react-transition-group";
+import avatar1 from "../images/avatar-default.png"
+import avatar2 from "../images/avatar-happy.png"
+import avatar3 from "../images/avatar-content.png"
 
 function Header() {
   let projects = useRouteMatch("/projects")
   let contact = useRouteMatch("/contact")
+  let home = useRouteMatch({
+    path: '/',
+    exact: true
+  })
+
   return (
     <header>
-        <h1>alexandra radevich</h1>
-        <div id="nav-wrapper">
-        <nav>
-        <div className="link-container">
-        { projects ? 
-            <Link className="link" to="/">Home</Link> : 
-            <Link className="link" to="/projects">Projects</Link> 
-        }
-        </div>
-        <div className="link-container">
-          <img id="avatar" src={avatar}></img>
-        </div>
-        <div className="link-container">
-        { contact ? 
-            <Link className="link" to="/">Home</Link> : 
-            <Link className="link" to="/contact">Contact</Link> 
-        }
-        </div>
-        </nav>
-        </div>
+      <TransitionGroup component={null}>
+        <CSSTransition classNames="selected" timeout={300}>
+          <Link to="/" id="home-link">{home? <h1><span className="selected">♥</span>alexandra radevich</h1> : <h1>alexandra radevich</h1>}</Link>
+          </CSSTransition>
+          <div id="nav-wrapper">
+          <nav>
+          <div className="link-container">
+              <Link className="link" to="/projects">{projects ? <><span className="selected">♥</span>Projects</> : 'Projects'}</Link> 
+          </div>
+          <div className="link-container">
+              <img id="avatar" src={ home ? avatar2 : contact ? avatar1 : avatar3 }></img>
+          </div>
+          <div className="link-container">
+              <Link className="link" to="/contact">{contact ? <><span className="selected">♥</span>Contact</> : 'Contact'}</Link> 
+          </div>
+          </nav>
+          </div>
+        </TransitionGroup>
     </header>
   );
 }
