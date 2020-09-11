3ffd4bb4702941e71b3a49ec6d33bd4486d40866
aleksarad <alexa.radevich1@gmail.com>
2020-09-11 15:59:09 -0400

    readme and media query updates

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This portfolio is a work in progress. Future plans include:
 
-* Moving over to Gatsby or NextJS
 * Continue to improve responsiveness/accessibility 
-* Adding a contact form
+* Fix h1 centering on smaller devices
+* Add animations
+
+### Resources Used ###
+* [avataaars](https://getavataaars.com/)
+* [icons8](icons8.com)
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,19 +15,19 @@ function Header() {
 
   return (
     <header>
-          <Link to="/" id="home-link">{home? <h1><span className="selected">♥</span>alexandra radevich<span className="heart-placeholder">♥</span></h1> : <h1><span className="heart-placeholder">♥</span>alexandra radevich<span className="heart-placeholder">♥</span></h1>}</Link>
+          <Link to="/" id="home-link">{ home? <h1><span className="selected">♥</span>alexandra radevich<span className="heart-placeholder">♥</span></h1> : <h1><span className="heart-placeholder">♥</span>alexandra radevich<span className="heart-placeholder">♥</span></h1>}</Link>
           <div id="nav-wrapper">
-          <nav>
-          <div className="link-container">
-              <Link className="link" to="/projects">{projects ? <><span className="selected">♥</span>Projects<span className="heart-placeholder">♥</span></> : <><span className="heart-placeholder">♥</span>Projects<span className="heart-placeholder">♥</span></>}</Link> 
-          </div>
-          <div className="link-container">
-              <img id="avatar" alt="cartoony avatar with blonde hair, grey crewneck on mint green background" src={ home ? avatar2 : contact ? avatar1 : avatar3 }></img>
-          </div>
-          <div className="link-container">
-              <Link className="link" to="/contact">{contact ? <><span className="selected">♥</span>Contact<span className="heart-placeholder">♥</span></> : <><span className="heart-placeholder">♥</span>Contact<span className="heart-placeholder">♥</span></>}</Link> 
-          </div>
-          </nav>
+            <nav>
+              <div className="link-container">
+                  <Link className="link" to="/projects">{projects ? <><span className="selected">♥</span>Projects<span className="heart-placeholder">♥</span></> : <><span className="heart-placeholder">♥</span>Projects<span className="heart-placeholder">♥</span></>}</Link> 
+              </div>
+              <div className="link-container">
+                  <img id="avatar" alt="cartoony avatar with blonde hair, grey crewneck on mint green background" src={ home ? avatar2 : contact ? avatar1 : avatar3 }></img>
+              </div>
+              <div className="link-container">
+                  <Link className="link" to="/contact">{contact ? <><span className="selected">♥</span>Contact<span className="heart-placeholder">♥</span></> : <><span className="heart-placeholder">♥</span>Contact<span className="heart-placeholder">♥</span></>}</Link> 
+              </div>
+            </nav>
           </div>
     </header>
   );
